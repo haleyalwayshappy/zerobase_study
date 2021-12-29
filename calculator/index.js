f7038270c747c86384418396b84168b4a89a4859
haleyalwayshappy <hongniest@gmail.com>
2021-12-29 22:26:57 +0900

    nocomments

--- calculator/index.js
+++ calculator/index.js
@@ -1,84 +1,102 @@
-const display = document.querySelector('.result');
-const numbers = document.querySelector('.numbers');
-const numBtn = numbers.querySelector('.number');
+const contents = document.querySelector('.contents');
+const display = document.querySelector('.result'); 
+const operators = document.querySelector('.operators');
 
-
-function calculate(num1, operator,num2) {
+function calculate(num1, operator, num2) {
     let result = 0;
+    /** if문  */
     if (operator === '+') {
         result = Number(num1) + Number(num2);
-        console.log('더하기 실행');
     } else if (operator === '-') {
         result = Number(num1) - Number(num2);
-    } else if (operator === '*') {
+    } else if (operator === '×') {
         result = Number(num1) * Number(num2);
-    } else if (operator === '/') {
+    } else if (operator === '÷') {
         result = Number(num1) / Number(num2);
-    } else if (operator === '=') {
-
-        if (firstNum !== '' && operatorForAdvanced === '') {
-            display.textContent = firstNum;
-        } else if (firstNum !== '' && previousNumber === '') {
-            display.textContent = calculate(display.textContent, operatorForAdvanced, display.textContent);
-        } else if (previousKey === display.textContent) {
-            display.textContent = calculate(firstNum, operatorForAdvanced, previousNumber);
-        } else if (previousKey !== display.textContent && previousNumber !== '') {
-            display.textContent = calculate(display.textContent, operatorForAdvanced, previousNumber);
-        } else if (previousKey !== display.textContent && previousNumber === '') {
-            display.textContent = firstNum;
-        }
     }
     return String(result);
 }
 
 
-let firstNum = '';
-let operatorForAdvanced = '';
-let previousKey = '';
-let previousNumber = '';
 
-numbers.addEventListener('click', function (event) {
-    const target = event.target;
-    const action = target.classList[0];
-    const buttonContent = target.textContent;
+let firstNum = ''; // 처음 숫자 
+let operatorForAdvanced = ''; // 연산식
+let previousKey = ''; // 이전 키 
+let previousNum = ''; // 이전숫자
+
+contents.addEventListener('click', function (event) {
+
+    const target = event.target; // 클릭된 html 엘리먼트의 정보가 저장되어 있다.
+    const action = target.classList[0]; // 클릭된 html 엘리먼트에 클래스 정보를 가져온다. 
+    const buttonContent = target.textContent; //클릭된 html 엘리먼트의 텍스트 정보를  가져온다. 
+    
 
     if (target.matches('button')) {
-        // 숫자를 눌렀을 때 화면 반영 처리
-        if (action === 'number') {
-            // 화면에 반영 되는값이 0일때 + 아무런 연산이 이루어지지 않았을 때 
-            if (display.textContent === '0' && operatorForAdvanced === '') {
-                display.textContent = buttonContent;
-                firstNum = display.textContent; // 첫번째 숫자를 화면에 반영한다. 
-            } else if (display.textContent !== '0' && operatorForAdvanced === '') {
-                // 연산 버튼이 비활성화 + 기존에 숫자가 입력되어있을때 뒤에 숫자를 추가해준다. 
-                display.textContent = display.textContent + buttonContent;
-                firstNum = display.textContent;
+        if (action === 'number') { // 번호 버튼 액션 => 번호가 화면에 차례대로 반영
+            if (display.textContent === '0' && operatorForAdvanced === '') { // 번호 + 연산자가 0 일경우 
+                display.textContent = buttonContent; // 숫자를 화면에 보여준다.
+                firstNum = display.textContent; // 화면에 보여지는 값을 firstNum 변수에 할당해준다. 
+            } else if (display.textContent !== '0' && operatorForAdvanced === '') { // 숫자는 이미 있지만 연산자는 '' 일 경우
+                display.textContent = display.textContent + buttonContent; //앞서 있던 숫자들과 새로 입력 받은 숫자를 '문자열'로 더해준다.
+                firstNum = display.textContent; // 화면에 보여지는 값을 firstNum 변수에 할당한다.
             } else if (display.textContent !== '0' && operatorForAdvanced !== '') {
                 if (previousKey === operatorForAdvanced) {
                     display.textContent = buttonContent;
-                    previousKey = display.textContent;
-                    console.log(previousKey);
-                    previousNumber = display.textContent;
+                    previousKey = display.textContent; // 직전키를 변수에 할당 
+                    previousNum = display.textContent; // 직전 숫자를 변수에 하당 
+                    // console.log('previousKey' + previousKey);
+                    // console.log('previousNum' + previousNum);
                 } else if (previousKey !== operatorForAdvanced) {
                     display.textContent = display.textContent + buttonContent;
-                    previousNumber = display.textContent;
+                    previousNum = display.textContent;
                 }
             }
         }
-        if (action === 'operator') {
-            operatorForAdvanced = buttonContent;
-            previousKey = operatorForAdvanced;
-        }
+
+        // 숫자 초기화 
         if (action === 'ac') {
             display.textContent = '0';
             firstNum = '';
-            previousNumber = '';
+            previousNum = '';
             operatorForAdvanced = '';
             previousKey = '';
         }
 
 
-    }
-});
+        if (action === 'operator') { // //클릭된 HTML 엘리먼트의 클래스 네임이 'operator'일때 분기     
+            operatorForAdvanced = buttonContent; // 오퍼레이터 누를 때 누른 텍스트 정보 할당
+            previousKey = operatorForAdvanced; // 직전키에 오퍼레이터 텍스트 정보 할당
+        }
+        
+        if (action == 'answer') { // Enter(계산) 버튼을 누를 때
+            if (firstNum !== '' && operatorForAdvanced === '') {
+                display.textContent = firstNum;
 
-// 연산 버튼 눌렀을때!
+            } else if (firstNum !== '' && previousNum === '') {
+                display.textContent = calculate(display.textContent, operatorForAdvanced, display.textContent)
+            } // 기존에 작성했던 calculate 함수를 이용하여 계산 상황에 따른 전달인자와 함께 호출
+
+            else if (previousKey === display.textContent) {
+                display.textContent = calculate(firstNum, operatorForAdvanced, previousNum)
+
+            } else if (previousKey !== display.textContent && previousNum !== '') {
+                display.textContent = calculate(display.textContent, operatorForAdvanced, previousNum)
+
+            } else if (previousKey !== display.textContent && previousNum === '') {
+                display.textContent = firstNum;
+
+            }
+
+        }
+    }
+
+
+
+
+
+    // 번호도 이미 있고, 연산자도 있을 경우 
+    // ac 버튼 액션 => 번호 및 연산자 초기화
+
+
+
+});
--- calculator/index4.js
+++ calculator/index4.js
@@ -1,65 +1,84 @@
-const calculators = document.querySelector('.calculators');
-const contents = document.querySelector('.contents');
-const display = document.querySelector('.result');
-const operators = document.querySelector('operators');
+/** 모법답안  
+ * 약간 별로인게 내가 풀었던 html에는 answer가 일관된 operator 였는데 모범답안에는 answer로 따로 클래스명이 설정되어있다. 
+ * 그리고 코드가 간단하네 _^^_^ 분석하자!
+ * 
+ */
+import {
+    $answerButton,
+    $clearButton,
+    $numbersWrapper,
+    $operatorsWrapper,
+    $result
+} from "./scripts/elements.js";
 
+let result = "";
 
-function add(firstNum, secondNum) {
-    return Number(firstNum + secondNum);
-}
+let leftOperand = "";
+let operator = "";
+let rightOperand = "";
 
-function minus(firstNum, secondNum) {
-    return Number(firstNum - secondNum);
-}
+const reset = () => {
+    leftOperand = "";
+    operator = "";
+    rightOperand = "";
+};
 
-function multiply(firstNum, secondNum) {
-    return Number(firstNum * secondNum);
-}
+const clearFormula = () => {
+    $result.innerText = "";
+    reset();
+};
 
-function divide(firstNum, secondNum) {
-    return Math.floor(firstNum / secondNum);
-}
-
-function clickDigits() {
-
-    let firstNum = ''; // 처음 숫자 
-    let operatorForAdvanced = ''; // 연산식
-    let previousKey = ''; // 이전 키 
-    let previousNum = ''; // 이전숫자
-    contents.addEventListener('click', function (event) {
-            const target = event.target; // 클릭된 html 엘리먼트의 정보가 저장되어 있다.
-            const action = target.classList[0]; // 클릭된 html 엘리먼트에 클래스 정보를 가져온다. 
-            const buttonContent = target.textContent; //클릭된 html 엘리먼트의 텍스트 정보를  가져온다. 
-
-            if (target.matches('button')) {
-                if (action === 'number') { // 번호 버튼 액션 => 번호가 화면에 차례대로 반영
-                    if (display.textContent === '0' && operatorForAdvanced === '') { // 번호 + 연산자가 0 일경우 
-                        display.textContent = buttonContent; // 숫자를 화면에 보여준다.
-                        firstNum = display.textContent; // 화면에 보여지는 값을 firstNum 변수에 할당해준다. 
-                    } else if (display.textContent !== '0' && operatorForAdvanced === '') { // 숫자는 이미 있지만 연산자는 '' 일 경우
-                        display.textContent = display.textContent + buttonContent; //앞서 있던 숫자들과 새로 입력 받은 숫자를 '문자열'로 더해준다.
-                        firstNum = display.textContent; // 화면에 보여지는 값을 firstNum 변수에 할당한다.
-                    } else if (display.textContent !== '0' && operatorForAdvanced !== '') {
-                        if (previousKey === operatorForAdvanced) {
-                            display.textContent = buttonContent;
-                            previousKey = display.textContent; // 직전키를 변수에 할당 
-                            previousNum = display.textContent; // 직전 숫자를 변수에 하당 
-                        } else if (previousKey !== operatorForAdvanced) {
-                            display.textContent = display.textContent + buttonContent;
-                            previousNum = display.textContent;
-                        }
-                    }
-                }
-
-                // 숫자 초기화 
-                if (action === 'ac') {
-                    display.textContent = '0';
-                    firstNum = '';
-                    previousNum = '';
-                    operatorForAdvanced = '';
-                    previousKey = '';
-                }
-
-            }
-        }
+$numbersWrapper.addEventListener("click", (event) => {
+    if (event.target.className !== "number") {
+        return;
     }
+
+    if (operator === "") {
+        leftOperand = event.target.innerText;
+    } else {
+        rightOperand = event.target.innerText;
+    }
+
+    $result.innerText = event.target.innerText;
+});
+
+$operatorsWrapper.addEventListener("click", (event) => {
+    if (event.target.className !== "operator") {
+        return;
+    }
+
+    if (result !== "" && leftOperand === "") {
+        leftOperand = result;
+    }
+
+    operator = event.target.innerText;
+});
+
+$clearButton.addEventListener("click", () => {
+    clearFormula();
+});
+
+$answerButton.addEventListener("click", () => {
+    switch (operator) {
+        case "+":
+            result = Number(leftOperand) + Number(rightOperand);
+            break;
+        case "-":
+            result = Number(leftOperand) - Number(rightOperand);
+            break;
+        case "*":
+            result = Number(leftOperand) * Number(rightOperand);
+            break;
+        case "/":
+            result = Number(leftOperand) / Number(rightOperand);
+            if (isFinite(result)) {
+                result = Math.floor(result);
+            }
+            break;
+        default:
+            break;
+    }
+
+    $result.innerText = result;
+    reset();
+});
