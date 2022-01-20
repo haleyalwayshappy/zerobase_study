38ce15dbe1d729a372399d49213431c7e0e8e7a8
haleyalwayshappy <hongniest@gmail.com>
2022-01-20 23:47:12 +0900

    todo List data

--- js-100-master/case1_todo_list/practice/script.js
+++ js-100-master/case1_todo_list/practice/script.js
@@ -1,12 +1,21 @@
-;(function () {
+;
+(function () {
   'use strict'
 
   const get = (target) => {
     return document.querySelector(target)
   }
 
+  const API_URL = 'http://localhost:3000/todos'
+  const $todos = get('.todos')
+  const $form = get('.todo_form')
+  const $todoInput = get('.todo_input')
+
   const createTodoElement = (item) => {
-    const { id, content } = item
+    const {
+      id,
+      content
+    } = item
     const $todoItem = document.createElement('div')
     $todoItem.classList.add('item')
     $todoItem.dataset.id = id
@@ -39,6 +48,46 @@
     return $todoItem
   }
 
-  const init = () => {}
+  const renderAllTodos = (todos) => {
+    $todos.innerHTML = ''
+    todos.forEach((item) => {
+      const todoElement = createTodoElement(item)
+      $todos.appendChild(todoElement)
+    })
+  }
+
+  const getTodos = () => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((todos) => renderAllTodos(todos))
+      .catch((error) => console.error(error))
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    const todo = {
+      content : $todoInput.value,
+      completed : false,
+    }
+    fetch(API_URL,{
+      method :'POST',
+      headers : {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todo),
+    }).then(getTodos)
+    .then(()=>{
+      $todoInput.value = ''
+      $todoInput.focus()
+    })
+    .catch((error)=>console.error(error))
+  }
+
+  const init = () => {
+    window.addEventListener('DOMContentLoaded', () => {
+      getTodos();
+    })
+    $form.addEventListener('submit', addTodo);
+  }
   init()
 })()
---
+++ js-today-house-main/README.md
@@ -0,0 +1,50 @@
+<img src='./images/readme_preview.png'>
+
+# 🪜 Javascript Today House Clone!
+
+## Before Start
+
+#### 📌 Local에서 쉽게 서버 띄워보기(Live Server)
+
+1. [Live Server](https://www.npmjs.com/package/live-server) 다운받기
+
+   ```
+     npm install -g live-server
+   ```
+
+2. 실행하기
+
+   ```
+     live-server 실행할_파일들이_있는_폴더명
+   ```
+
+#### 📌 Local에서 데이터베이스 서버 띄워보기(Json Server)
+
+1. json-server 설치하기
+
+   ```
+     npm install json-server
+   ```
+
+2. json-server 실행하기
+
+   ```
+     npm run serve
+   ```
+
+<b>(+) [VSCode Extension](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)으로도 사용해볼 수 있어요!</b>
+
+## How To Start?
+
+1. 이 레포지토리를 Clone 받으세요.
+2. npm install 을 통해 필요한 패키지를 다운받으세요
+3. Live Server를 실행하세요.
+4. Json Server를 실행하세요.
+
+## Bug
+
+버그가 있다면 [Issues](https://github.com/ddongule/js-today-house/issues)에 등록해주세요!
+
+## License
+
+이 프로젝트는 [MIT License](https://github.com/ddongule/js-today-house/blob/main/LICENSE)를 따릅니다.
