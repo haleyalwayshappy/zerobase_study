af793f398fd71b32eeb541fa20445fb576810a23
haleyalwayshappy <hongniest@gmail.com>
2022-01-21 00:24:39 +0900

    error found

--- js-100-master/case1_todo_list/practice/script.js
+++ js-100-master/case1_todo_list/practice/script.js
@@ -1,22 +1,20 @@
-;
-(function () {
+;(function () {
   'use strict'
 
   const get = (target) => {
     return document.querySelector(target)
   }
 
-  const API_URL = 'http://localhost:3000/todos'
   const $todos = get('.todos')
   const $form = get('.todo_form')
   const $todoInput = get('.todo_input')
+  const API_URL = `http://localhost:3000/todos`
 
   const createTodoElement = (item) => {
-    const {
-      id,
-      content
-    } = item
+    const { id, content, completed } = item
+    const isChecked = completed ? 'checked' : ''
     const $todoItem = document.createElement('div')
+
     $todoItem.classList.add('item')
     $todoItem.dataset.id = id
     $todoItem.innerHTML = `
@@ -59,35 +57,58 @@
   const getTodos = () => {
     fetch(API_URL)
       .then((response) => response.json())
-      .then((todos) => renderAllTodos(todos))
-      .catch((error) => console.error(error))
+      .then((todos) => {
+        renderAllTodos(todos)
+      })
+      .catch((error) => console.error(error.message))
   }
 
   const addTodo = (e) => {
-    e.preventDefault();
+    e.preventDefault()
+    const content = $todoInput.value
+    if (!content) return
+
     const todo = {
-      content : $todoInput.value,
-      completed : false,
+      content,
+      completed: false,
     }
-    fetch(API_URL,{
-      method :'POST',
-      headers : {
-         'Content-Type': 'application/json'
-      },
+    fetch(API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(todo),
-    }).then(getTodos)
-    .then(()=>{
-      $todoInput.value = ''
-      $todoInput.focus()
     })
-    .catch((error)=>console.error(error))
+      .then((response) => response.json())
+      .then(getTodos)
+      .then(() => {
+        $todoInput.value = ''
+        $todoInput.focus()
+      })
+      .catch((error) => console.error(error.message))
+  }
+
+  const toggleTodo = (e) => {
+    if (e.target.className !== 'todo_checkbox') return
+    const $item = e.target.closest('.item')
+    const id = $item.dataset.id
+    const completed = e.target.checked
+    fetch(`${API_URL}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ completed }),
+    })
+      .then((response) => response.json())
+      .then(getTodos)
+      .catch((error) => console.error(error.message))
   }
 
   const init = () => {
     window.addEventListener('DOMContentLoaded', () => {
-      getTodos();
+      getTodos()
     })
-    $form.addEventListener('submit', addTodo);
+    $form.addEventListener('submit', addTodo)
+    $todos.addEventListener('click', toggleTodo)
   }
   init()
-})()
+})
