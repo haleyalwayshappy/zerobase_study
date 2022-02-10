6fd442f216ba6e079b0e66078bba211e10f45e7f
haleyalwayshappy <hongniest@gmail.com>
2022-02-11 03:15:52 +0900

    baseball game markup

--- js-100-master/case3_baseball/practice/script.js
+++ js-100-master/case3_baseball/practice/script.js
@@ -1,3 +1,21 @@
-;(function () {
+;(() => {
   'use strict'
+  const get = (target) => document.querySelector(target)
+
+  const init = () => {
+    get('form').addEventListener('submit',(event)=>{
+      playGame(event)
+    })
+    setPassword()
+  }
+
+  const baseball = {
+    limit :10,
+    digit : 4,
+    trial : 0 ,
+    end : false,
+    $question : get('.ball_question'),
+    $answer : get('.ball_answer'),
+    $input : get('.ball_input'),
+  } 
 })()
